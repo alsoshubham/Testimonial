fd06a5310f2180b1f840108ececd7e081839f04e
alsoshubham <shubhamsahu100100@hotmail.com>
2024-09-13 21:54:05 +0530

    .

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,26 +6,9 @@ export const Card = () => {
         </div>
         <div>
             <p>
-
+            
             </p>
         </div>
-    
     </div>
   );
 };
-
-
-// <div
-// style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", gap: 20 }}>
-// <div>
-//   <h3>Shubham Shah</h3>
-//   <h4>Frontend Developer</h4>
-// </div>
-// </div>
-// <p>
-// Shubham is an exceptional frontend developer. His attention to detail
-// and dedication to creating seamless user experiences have significantly
-// improved our product. He consistently delivers high-quality work and is
-// a pleasure to work with.
-// </p>
-// <button>Surprise Me</button>
