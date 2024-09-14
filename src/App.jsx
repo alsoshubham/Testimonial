64eae17032a5705bf1e207efa6939a0029113c50
alsoshubham <shubhamsahu100100@hotmail.com>
2024-09-14 23:55:57 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Testimonials } from './components/Testimonial';
 import { reviews } from './data';
+import './index.css';
 
 function App() {
   return (
-    <div>
-      <div>
-        <h1>
+    <div className='flex flex-col w-[100vww] h-[100vh] justify-center items-center bg-gray-200'>
+      <div className='text-center'>
+        <h1 className='text-4xl font-bold'>
           Our Testimonial
         </h1>
-        <div></div>
+        <div className='bg-violet-400 h-[4px] w-[1/5] mt-1'></div>
         <Testimonials reviews={reviews}/>
 
       </div>
